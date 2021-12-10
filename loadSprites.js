24f2a51c6786da0afead9470f524df418022eda0
clarice <klaritatis@gmail.com>
2021-12-10 00:31:41 -0300

    clean stuff

--- loadSprites.js
+++
@@ -1,5 +0,0 @@
-loadSprite("square", "sprites/s.png");
-loadSprite("circle", "sprites/c.png");
-loadSprite("triangle", "sprites/t.png");
-loadSprite("redD", "sprites/red-dot.png");
-loadSprite("clear", "sprites/clear.PNG");
